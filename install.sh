80dd3e9482fe13a3dcd72e562aeacb0421a05dd4
Jason Kulatunga <jason@thesparktree.com>
2020-12-12 10:42:54 -0800

    set the install script.

--- install.sh
+++ install.sh
@@ -2,29 +2,14 @@
 set -e
 # This script must be executed on the flatcar host, not within a container
 
-# TODO: check if we can verify that this is running in the correct context (host vs container)
+FORKLIFT_LD_ROOT=${FORKLIFT_LD_ROOT:-/}
 
-local DRIVER_NAME=$1
-local DRIVER_VERSION=$2
-mkdir -p "$MODULUS_INSTALL_DIR"
-rm -rf "${MODULUS_INSTALL_DIR:?}"/"$DRIVER_NAME"
-ln -s "$MODULUS_CACHE_DIR"/"$DRIVER_NAME"/"$DRIVER_VERSION" "$MODULUS_INSTALL_DIR"/"$DRIVER_NAME"
-
-if [ -d "$MODULUS_INSTALL_DIR"/"$DRIVER_NAME"/lib ] ; then
-    mkdir -p "$MODULUS_LD_ROOT"/etc/ld.so.conf.d
-    echo "$MODULUS_INSTALL_DIR"/"$DRIVER_NAME"/lib > "$MODULUS_LD_ROOT"/etc/ld.so.conf.d/"$DRIVER_NAME".conf
-    ldconfig -r "$MODULUS_LD_ROOT" 2> /dev/null
-fi
-# shellcheck disable=SC1090
-source "$MODULUS_BIN_DIR"/"$DRIVER_NAME"/install
-
-
-depmod -b "$MODULUS_LD_ROOT"
+depmod -b "$FORKLIFT_LD_ROOT"
 # This is an NVIDIA dep that is not specified in the module.dep file.
-modprobe -d "$MODULUS_LD_ROOT" ipmi_devintf
-depmod -b "$MODULUS_INSTALL_DIR/$DRIVER_NAME"
-modprobe -d "$MODULUS_INSTALL_DIR/$DRIVER_NAME" nvidia
-modprobe -d "$MODULUS_INSTALL_DIR/$DRIVER_NAME" nvidia-uvm
+modprobe -d "$FORKLIFT_LD_ROOT" ipmi_devintf
+depmod -b .
+modprobe -d . nvidia
+modprobe -d . nvidia-uvm
 if [ ! -e /dev/nvidia0 ] ; then
     NVDEVS=$(lspci | grep -i NVIDIA)
     N3D=$(echo "$NVDEVS" | grep -c "3D controller") || true
